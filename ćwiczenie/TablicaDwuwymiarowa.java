adbecbe40b8dbef6c328f52d76c26b2147f211c5
SawicaKica <77607418+SawicaKica@users.noreply.github.com>
2021-03-01 09:10:53 +0100

    Stworzenie tablicy dwuwymiarowej

--- ćwiczenie/TablicaDwuwymiarowa.java
+++ ćwiczenie/TablicaDwuwymiarowa.java
@@ -1,8 +1,10 @@
 
 class TablicaDwuwymiarowa{
 	public static void main(String [] args){
-	int [] tab = new int[5];
-	tab[0] = 40;
-	tab[1] = "jeden";
-	}
+		int [] tab = new int[5];
+		tab[0] = 40;
+		for(int i=0;i<tab.length;i++){
+			System.out.println(tab[i]);
+		}
+		int [][] tab2d = new int[5][2];
 }
