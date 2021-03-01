e185a04215811dfe830f4f79354df272c43cd7e6
SawicaKica <77607418+SawicaKica@users.noreply.github.com>
2021-03-01 09:27:09 +0100

    przeniesienie funkcjonalnosci do metody tab2dFirst

--- ćwiczenie/TablicaDwuwymiarowa.java
+++ ćwiczenie/TablicaDwuwymiarowa.java
@@ -1,10 +1,21 @@
 
 class TablicaDwuwymiarowa{
-	public static void main(String [] args){
-		int [] tab = new int[5];
-		tab[0] = 40;
-		for(int i=0;i<tab.length;i++){
-			System.out.println(tab[i]);
-		}
-		int [][] tab2d = new int[5][2];
+    public static void main(String [] args){ 
+        int [] tab = new int[5];
+        tab[0] = 40;
+        for(int i=0;i<tab.length()i++){
+            System.out.println(tab[i]);
+        }    
+        int[][] tab2b = new int[5][2]; 
+        tab2d[0][1] = 50;
+        tab2d[2][1] = -200;
+        tab2d[4][0] = 444;  
+        for (int i=0;i<tab2d.length;i++){
+            for(int j=0;j<tab2d[i].length;j++){
+                System.out.println(tab2d[i][0]+" "+tab2d[i][1]);
+            }
+            System.out.println("Koniec pętli ze zmienna j");
+        }
+        System.out.println("Koniec pętli ze zmienną i");
+    }
 }
