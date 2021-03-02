138de32ddc0204cbc69f540df1e24a13112b27f8
SawicaKica <77607418+SawicaKica@users.noreply.github.com>
2021-03-02 10:32:05 +0100

    Zadanko na lekcji

--- 02.03.2021/dwuwymiarowe.java
+++ 02.03.2021/dwuwymiarowe.java
@@ -1,5 +1,8 @@
-class TabliceDwuwymiarowa{
+class Dwuwymiarowa{
     public static void main(String [] args){
+		tablicaMnozenia();
+	}
+	private static void tablica2x2(){
 		int[][] tab = new int[2][2];
         tab[0][0] = 1;
 		tab[0][1] = 2;
@@ -7,8 +10,21 @@ class TabliceDwuwymiarowa{
 		tab[1][1] = 4;
 		for (int i=0;i<tab.length;i++){
             for(int j=0;j<tab[i].length;j++){
-                System.out.println(tab[i][j]+" "+tab[i][j]);			
+                System.out.println(tab[i][j]);			
 			}
 		}
 	}
-}
+	private static void tablicaMnozenia(){
+        int  tab [][] = new int[10][10];
+        for (int i=0;i<tab.length;i++){
+            for(int j=0;j<tab[i].length;j++){
+                tab [i][j] = (i+1)*(j+1);
+            }
+        }
+        for (int i=0;i<tab.length;i++){
+            for(int j=0;j<tab[i].length;j++){
+                System.out.print(tab[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
