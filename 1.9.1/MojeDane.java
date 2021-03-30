0e3672c18a4bc431758cd83d05999362fc130eb7
SawicaKica <77607418+SawicaKica@users.noreply.github.com>
2021-03-30 09:08:05 +0200

    30.03.2021

---
+++ 1.9.1/MojeDane.java
@@ -0,0 +1,11 @@
+class MojeDane{
+	public String imie = "Jakub";
+	public String nazwisko = "Sawicki";
+	public String klasa = "2PTN";
+	
+	public MojeDane(String imie, String nazwisko, String klasa){
+		this.imie = imie;
+		this.nazwisko = nazwisko;
+		this.klasa = klasa;
+	}
+}
