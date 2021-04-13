a0537a828af578b8288e19e9eebce5951f5134c0
SawicaKica <77607418+SawicaKica@users.noreply.github.com>
2021-04-13 10:03:21 +0200

    Działa!

--- Wiele konstruktorów/Trojkat.java
+++ Wiele konstruktorów/Trojkat.java
@@ -2,17 +2,17 @@ class Trojkat{
     private double a = 0;
     private double b = 0;
     private double c = 0;
-    private double h = 0;
+    private double wysokosc = 0;
     
     public Trojkat(double a,double b,double c,double h ){
         this.a = a;
         this.b = b;
         this.c = c;
-        this.h = h;
+        this.wysokosc = wysokosc;
     }
     
     @Override
     public String toString(){
-        return "Prostokatny: "+(a*h/2)+"\nrownoboczny: "+((a*a)*Math.pow(a, 1/3))/4;
+        return "Prostokatny: "+(a*wysokosc/2)+"\nrownoboczny: "+((a*a)*Math.pow(a, 1/3))/4;
     }
 }
